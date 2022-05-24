399257afd251968273ad741dcae07700af9fac38
Zixuan Liu <nodeces@gmail.com>
2022-05-24 10:05:24 +0800

    Fix comment

--- hashcode/hashcode.go
+++ hashcode/hashcode.go
@@ -6,12 +6,12 @@ import (
 	"hash/crc32"
 )
 
-// Implementation fro https://www.terraform.io/docs/extend/guides/v2-upgrade-guide.html
+// Implementation from https://www.terraform.io/docs/extend/guides/v2-upgrade-guide.html
 
 // String hashes a string to a unique hashcode.
 //
-// crc32 returns a uint32, but for our use we need
-// and non negative integer. Here we cast to an integer
+// crc32 returns an uint32, but for our use we need
+// a non-negative integer. Here we cast to an integer
 // and invert it if the result is negative.
 func String(s string) int {
 	v := int(crc32.ChecksumIEEE([]byte(s)))
