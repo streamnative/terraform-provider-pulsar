0cd31421212042f1b6165b6d605435b2ede04313
Neng Lu <nlu@streamnative.io>
2023-07-27 16:12:01 -0700

    upgrade pulsar

--- hack/pulsarimage/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export PULSAR_PREFIX_systemTopicEnabled=true
-export PULSAR_PREFIX_topicLevelPoliciesEnabled=true
-
-python3 /pulsar/bin/apply-config-from-env.py /pulsar/conf/standalone.conf
-
-/pulsar/bin/pulsar standalone
