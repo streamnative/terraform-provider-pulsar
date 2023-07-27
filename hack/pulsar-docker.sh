dd178319df0d6a0425e684835ebcad0c8940c9f5
Neng Lu <nlu@streamnative.io>
2023-07-27 13:56:30 -0700

    update

--- hack/pulsar-docker.sh
+++ hack/pulsar-docker.sh
@@ -13,7 +13,7 @@ run)
   docker build -t ${IMAGE} -f hack/pulsarimage/Dockerfile hack/pulsarimage
   docker run -d -p 6650:6650 -p 8080:8080 --name ${CONTAINER} ${IMAGE}
   until curl http://localhost:8080/admin/v2/tenants >/dev/null 2>&1; do
-    sleep 1
+    sleep 5
     echo "Wait for pulsar service to be ready...$(date +%H:%M:%S)"
   done
   echo "Pulsar service is ready"
